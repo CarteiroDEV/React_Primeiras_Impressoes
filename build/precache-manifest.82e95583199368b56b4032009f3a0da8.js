b99fa14106cf7c94de5b3d11d8a059168effa881
CarteiroDEV <44775579+CarteiroDEV@users.noreply.github.com>
2019-04-01 13:30:37 -0300

    REACT build

---
+++ build/precache-manifest.82e95583199368b56b4032009f3a0da8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e366d549202099f7b416",
+    "url": "/static/js/main.7cc4581d.chunk.js"
+  },
+  {
+    "revision": "254a14c09bfef11176c1",
+    "url": "/static/js/2.ccc89068.chunk.js"
+  },
+  {
+    "revision": "e366d549202099f7b416",
+    "url": "/static/css/main.e4453704.chunk.css"
+  },
+  {
+    "revision": "048c43b737454bc4fb642db52eb7feb4",
+    "url": "/index.html"
+  }
+];
